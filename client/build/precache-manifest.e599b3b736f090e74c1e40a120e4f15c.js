34ff1741cedc0b1e1978ca1d097960e06dbc21b7
bloom510 <bloomjazzer@gmail.com>
2018-12-26 09:51:39 -0800

    deploy

---
+++ client/build/precache-manifest.e599b3b736f090e74c1e40a120e4f15c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "48b147260e88aafe1488",
+    "url": "/static/js/main.48b14726.chunk.js"
+  },
+  {
+    "revision": "26530183ca75e288d8af",
+    "url": "/static/js/1.26530183.chunk.js"
+  },
+  {
+    "revision": "48b147260e88aafe1488",
+    "url": "/static/css/main.bd645c55.chunk.css"
+  },
+  {
+    "revision": "26530183ca75e288d8af",
+    "url": "/static/css/1.8fce1ca6.chunk.css"
+  },
+  {
+    "revision": "90196f7cd6a7578fce32b3af1a49c585",
+    "url": "/index.html"
+  }
+];
