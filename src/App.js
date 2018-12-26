93ffda202a205130100c153b0694a8ed7b270465
bloom510 <bloomjazzer@gmail.com>
2018-12-25 17:18:28 -0800

    Adds antd UI

--- src/App.js
+++ src/App.js
@@ -1,30 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Menu, Dropdown, Icon, Button } from 'antd';
+
+
 
 class App extends Component {
   constructor() {
     super();
+    this.state = {
+      value: ''
+    }
     this.actx = new AudioContext();
+    this.handleChange = this.handleChange.bind(this);
   }
-  componentDidMount() {
-
+  handleChange(e) {
+    console.log('click ', e.key);
+    this.setState({
+      current: e.key,
+    });
   }
   render() {
+    const menu = (
+      <Menu onClick={(e) => this.handleChange(e)}>
+        <Menu.Item key="bypass">
+          Bypass Filter
+        </Menu.Item>
+        <Menu.Item key="filter">
+          One Pole Filter
+        </Menu.Item>
+        <Menu.Item key="noise">
+          Noise
+        </Menu.Item>
+        <Menu.Item key="bitcrusher">
+          Bitcrusher
+        </Menu.Item>   
+        <Menu.Item key="messageport">
+          Message Port
+        </Menu.Item>  
+      </Menu>
+    );
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           React + AudioWorklet = ❤
-          <p>
-          <select>
-            <option value="bypasser">Bypasser</option>
-            <option value="filter">One Pole Filter</option>
-            <option value="noise">Noise Generator</option>
-            <option value="bitcrusher">Bit Crusher</option>
-            <option value="message">Message Port</option>
-          </select>
-          </p>
+          <div style={{float:'left', width: '100%'}}>
+            <Dropdown overlay={menu} size='small'>
+              <a className="ant-dropdown-link" href="#">
+                Select a module <Icon type="down" />
+              </a>
+            </Dropdown>
+            <Button ghost style={{marginLeft:'1%'}}>Start</Button>
+          </div>
         </header>
       </div>
     );
