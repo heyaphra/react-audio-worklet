4ddb3128eefcba8300a3284cfd2420ba695b1136
bloom510 <bloomjazzer@gmail.com>
2018-12-25 18:27:12 -0800

    Adds switch logic

--- src/App.js
+++ src/App.js
@@ -9,21 +9,13 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      current: 'Select a module'
+      selected: null,
+      modulesLoaded: false,
+      isPlaying: false
     }
-    this.actx = new AudioContext();
-    this.handleChange = this.handleChange.bind(this);
-  }
-  handleChange(e) {
-    console.log('click ', e.key);
-    this.setState({
-      current: e.key,
-    });
-  }
-  render() {
-    const menu = (
-      <Menu onClick={(e) => this.handleChange(e)} selectedKeys={[this.state.current]}>
-        <Menu.Item key="Bypass Filter">
+    this.menu = (
+      <Menu onClick={(e) => this.handleSelect(e)} selectedKeys={[this.state.current]}>
+        <Menu.Item key="Bypass Filter" custom='prop'>
           Bypass Filter
         </Menu.Item>
         <Menu.Item key="One Pole Filter">
@@ -40,6 +32,72 @@ class App extends Component {
         </Menu.Item>  
       </Menu>
     );
+    this.bypassProcessor = this.bypassProcessor.bind(this);
+    this.handleSelect = this.handleSelect.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+
+  }
+  async loadModules() {
+    const { actx } = this;    
+    await actx.audioWorklet.addModule(
+        `${process.env.PUBLIC_URL}/worklet/bypass-processor.js`
+    );
+    this.setState({modulesLoaded: true})
+    console.log('loaded modules!')
+  }
+  bypassProcessor() {
+    const { actx } = this;
+    const bypasser = new AudioWorkletNode(actx, 'bypass-processor');
+    this.oscillator = new OscillatorNode(actx);                
+    this.oscillator.connect(bypasser).connect(actx.destination);
+    this.oscillator.start();
+  }
+  handleSelect(e) {
+    this.setState({selected: e.key});
+  }
+  async handleClick() {
+    const { state } = this;
+
+    if(state.selected) {
+      this.setState({isPlaying: !state.isPlaying});    
+    }    
+    
+    if(!this.actx) {
+      try {
+        console.log('New context instantiated')
+        this.actx = new (window.AudioContext || window.webkitAudioContext)();
+        await this.loadModules();
+      } catch(e) {
+        console.log(`Sorry, but your browser doesn't support the Web Audio API!`);
+      }
+    }
+
+    switch (state.selected) {
+      case 'Bypass Filter':
+        if(state.isPlaying) {
+          this.oscillator.stop();
+        } else {
+          this.bypassProcessor();
+        }
+      break;
+      case 'One Pole Filter':
+        console.log('pole');
+      break;
+      case 'Noise':
+        console.log('noise')
+      break;
+      case 'Bitcrusher':
+        console.log('crushit');
+      break;
+      case 'Message Port':
+        console.log('postin')
+      break;
+    }
+    
+
+  }
+  render() {
+    const { state, menu } = this;
     return (
       <div className="App">
         <header className="App-header">
@@ -48,10 +106,10 @@ class App extends Component {
           <div style={{float:'left', width: '100%'}}>
             <Dropdown overlay={menu} size='small'>
               <a className="ant-dropdown-link" href="#">
-                {this.state.current} <Icon type="down" />
+                {state.selected ? state.selected : 'Select a module'} <Icon type="down" />
               </a>
             </Dropdown>
-            <Button ghost style={{marginLeft:'1%'}}>Start</Button>
+            <Button ghost onClick={() => this.handleClick()} style={{marginLeft:'1%'}}>{state.isPlaying ? 'Stop' : 'Start'}</Button>
           </div>
         </header>
       </div>
