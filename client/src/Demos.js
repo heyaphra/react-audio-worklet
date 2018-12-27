101e75dde027da02d83537690f2cb7f3920622d9
bloom510 <bloomjazzer@gmail.com>
2018-12-27 10:57:58 -0800

    Some cleanup

--- client/src/Demos.js
+++ client/src/Demos.js
@@ -8,7 +8,6 @@ export const Bypasser = (App) => {
     oscillator.start();
     return bypasserNode;
 }
-
 /* The example below initially demonstrated a one-off scheduled event. I've modified it to play
     based on the AudioContext's currentTime so that it can be replayed at the press of a button. 
     It creates a new AudioWorkletNode and a new oscillator, connects the new oscillator to the 
@@ -29,11 +28,7 @@ export const onePoleFilter = (App) => {
         oscillator.connect(filterNode).connect(actx.destination);
         oscillator.start();
         return filterNode;
-        // If I want to implement the below method without UI glitches, time demarcations must be managed in App state.
-        // this.oscillator.onended = () => {
-        //     this.setState({ isPlaying: false })
-        // }
-      }
+}
 
 export const noiseGenerator = (App) => {
     const { actx } = App;
@@ -42,12 +37,10 @@ export const noiseGenerator = (App) => {
     const noiseGeneratorNode = new AudioWorkletNode(actx, 'noise-generator');
     const paramAmp = noiseGeneratorNode.parameters.get('amplitude');
     noiseGeneratorNode.connect(actx.destination);
-    // Connect the oscillator to 'amplitude' AudioParam.
     modulator.connect(modGain).connect(paramAmp);
     modulator.frequency.value = 0.5;
     modGain.gain.value = 0.75;
     modulator.start();
-
     return noiseGeneratorNode;
 }
 export const bitCrusher = (App) => {
@@ -62,12 +55,9 @@ export const bitCrusher = (App) => {
     oscillator.type = 'sawtooth';
     oscillator.frequency.value = 5000;
     oscillator.connect(bitCrusherNode).connect(actx.destination);
-    // Play the tone for 8 seconds.
     oscillator.start();
     oscillator.stop(end);
     paramBitDepth.setValueAtTime(1, 0);
-    // |frequencyReduction| parameters will be automated and changing over
-    // time. Thus its parameter array will have 128 values.
     paramReduction.setValueAtTime(0.01, beginning);
     paramReduction.linearRampToValueAtTime(0.1, middle);
     paramReduction.exponentialRampToValueAtTime(0.01, end);
